e6633d45a5da76aad6ce60e58615d4baaa5bd608
rl8384 <radoslaw.lis@dxc.com>
2022-12-07 02:39:06 +0100

    Fix error with parsing as a list

--- scrabble_app/game_logic/move_parser.py
+++ scrabble_app/game_logic/move_parser.py
@@ -37,12 +37,37 @@ def parse_move(move_string, country):
         orientation = Orientation.VERTICAL
     else:
         raise IncorrectMoveError("Move should be formatted like this - 8:A:KNIFE or B:13:TREE")
-    for letter in letters:
+    letters_list = parse_letters_to_list(country, letters)
+    for letter in letters_list:
         if letter not in utils.legal_letters[country.name]:
             raise IncorrectMoveError(f"Move should contain only letters from {country.name} dictionary")
     logger.info(f"Move parsed, x coord = {x_coord}, y coord = {y_coord}, letters = {letters}, orientation = {orientation}")
-    return Move(x_coord, y_coord, letters, orientation, move_string, country)
+    return Move(x_coord, y_coord, letters_list, orientation, move_string, country)
 
+
+def parse_letters_to_list(country, letters):
+    letters = letters.lower()
+    logger.debug(f"Parsing letters string - {letters}")
+    letters_list = []
+    skip_next = False
+    for index, letter in enumerate(letters):
+        if skip_next:
+            skip_next = False
+            continue
+        if country.name == "ES" and index < len(letters) - 1:
+            letter, skip_next = check_if_contains_spanish_doubles(letters, index)
+        letters_list.append(letter)
+    logger.debug(f"Parsed letters string - {letters_list}")
+    return letters_list
+
+
+def check_if_contains_spanish_doubles(word, index):
+    word = word.lower()
+    spanish_doubles = ['ll', 'rr', 'ch']
+    if (double := word[index:index + 2]) in spanish_doubles:
+        return double, True
+    else:
+        return word[index].lower(), False
 
 class Replace:
     def __init__(self, letters, github_user, issue_title, issue_number):
@@ -57,9 +82,9 @@ class Replace:
 
 
 class Move:
-    def __init__(self, x_start, y_start, letters_string, orientation, move_string, country):
+    def __init__(self, x_start, y_start, letters_list, orientation, move_string, country):
         self.tiles = []
-        for index, letter in enumerate(letters_string.upper()):
+        for index, letter in enumerate([letter.upper() for letter in letters_list]):
             x = x_start if orientation == Orientation.HORIZONTAL else x_start + index
             y = y_start if orientation == Orientation.VERTICAL else y_start + index
             if y > 14 or x > 14:
