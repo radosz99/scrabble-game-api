4f65c73ecac3b0f4a4a57d1a3e52a0b54d5b500f
rl8384 <radoslaw.lis@dxc.com>
2022-12-07 01:04:58 +0100

    Improve logging

--- scrabble_app/logger.py
+++ scrabble_app/logger.py
@@ -1,13 +1,28 @@
 import logging
 
-LOG_FILE = "debug.log"
-
-logging.basicConfig(format='%(asctime)s,%(msecs)d (%(filename)s, %(lineno)d) %(name)s %(levelname)s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=[
-                        logging.FileHandler(LOG_FILE, 'w+'),
-                        logging.StreamHandler()
-                    ],
-                    level=logging.INFO)
+DEBUG_LOG_FILE = "debug.log"
+INFO_LOG_FILE = "info.log"
 
 logger = logging.getLogger("root")
+
+
+formatter = logging.Formatter(
+    '%(asctime)s,%(msecs)d (%(filename)s, %(lineno)d) %(name)s %(levelname)s %(message)s')
+
+logger.setLevel(logging.DEBUG)
+
+file_handler_info = logging.FileHandler(INFO_LOG_FILE, 'w+')
+file_handler_info.setLevel(logging.INFO)
+file_handler_info.setFormatter(formatter)
+
+file_handler = logging.FileHandler(DEBUG_LOG_FILE, 'w+')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(file_handler_info)
+logger.addHandler(console_handler)
